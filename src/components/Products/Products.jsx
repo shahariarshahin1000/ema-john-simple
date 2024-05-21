267a77ed42a623abb623e8872a0336411e91b51b
shahariar shahin <shahariarshahin1000@gmail.com>
2024-05-21 14:11:34 +0600

    frenish ema john simple

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,20 +1,32 @@
 // import React from 'react';
-import './Products.css'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faShoppingCart } from "@fortawesome/free-solid-svg-icons";
+import "./Products.css";
 
 const Products = (props) => {
-     const {img, price, name, seller,ratings} =props.product;
-    return (
-        <div className='product'>
-            <img src={img} alt="" />
-           <div className='product-info'>
-             <h6 className='product-name'>{name}</h6>
-             <p>Price: ${price}</p>
-             <p>Manufacturer:{seller}</p>
-             <p>Rating:{ratings} Stars</p>
-           </div>
-           <button className='btn-cart'>Add to Cart</button>
+  const { img, price, name, seller, ratings } = props.product;
+
+  const handleAddToCart = props.handleAddToCart;
+  return (
+    <div className="product">
+      <div>
+        <img src={img} alt="" />
+        <div className="product-info">
+          <h6 className="product-name">{name}</h6>
+          <p>Price: ${price}</p>
+          <p>Manufacturer:{seller}</p>
+          <p>Rating:{ratings} Stars</p>
         </div>
-    );
+      </div>
+      <button
+        onClick={() => handleAddToCart(props.product)}
+        className="btn-cart"
+      >
+        Add to Cart
+        <FontAwesomeIcon icon={faShoppingCart} />
+      </button>
+    </div>
+  );
 };
 
 export default Products;
--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -1,30 +1,38 @@
-import { useState } from 'react';
-import './Shop.css';
-import { useEffect } from 'react';
-import Products from '../Products/Products';
+import { useState } from "react";
+import "./Shop.css";
+import { useEffect } from "react";
+import Products from "../Products/Products";
 const Shop = () => {
-    const [products, setProducts]=useState([])
+  const [products, setProducts] = useState([]);
+  const [cart, setCart] = useState([]);
 
-    useEffect(()=>{
-        fetch('products.json')
-        .then(res => res.json())
-        .then(data =>setProducts(data));
-    },[])
-    return (
-        <div className='shop-container'>
-            <div className="products-container">
-              {
-                products.map(product =><Products
-                key={product}
-                product={product}
-                ></Products> )
-              }
-            </div>
-            <div className="cart-container">
-                <h4>Order sumary</h4>
-            </div>
-        </div>
-    );
+  useEffect(() => {
+    fetch("products.json")
+      .then((res) => res.json())
+      .then((data) => setProducts(data));
+  }, []);
+
+  const handleAddToCart = (product) => {
+    const newCart = [...cart, product];
+    setCart(newCart);
+  };
+  console.log(cart);
+  return (
+    <div className="shop-container">
+      <div className="products-container">
+        {products.map((product) => (
+          <Products
+            key={product?.id}
+            product={product}
+            handleAddToCart={handleAddToCart}
+          ></Products>
+        ))}
+      </div>
+      <div className="cart-container">
+        <h4>Order sumary</h4>
+        <p>Selected Item:{cart.length}</p>
+      </div>
+    </div>
+  );
 };
-
 export default Shop;
